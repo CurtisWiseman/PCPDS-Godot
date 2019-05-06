28097a84a4b71cf204cd11d556c78c2171640258
remedee <zacharydanieldowns@gmail.com>
2019-05-05 23:23:19 -0500

    Video Background Support
    Backgrounds can now also be videos! Videos have to be in the ogv format or they will stutter when they loop because godot only properly supports ogv for some reason.

--- mainmenu/scripts/settings.gd
+++
@@ -1,9 +0,0 @@
-extends Node
-
-var systems # Define systems for use globally.
-
-func _ready():
-	systems = get_node('Systems') # Load systems with the Systems node.
-	
-	# Display the main_menu.png for the settings background.
-	systems.display.background('res://mainmenu/main_menu.png', get_node('.'))
--- scripts/display.gd
+++ scripts/display.gd
@@ -3,20 +3,35 @@ extends Sprite
 var bgnode # The background image node.
 
 # Make the given image 'bg' a background.
-func background(bg, node):
+func background(bg, type, node):
 	
 	bg = load(bg) # Load the provided image path.
-	
+	 
 	# If a background is already set then remove it.
-	#if is_a_parent_of(get_node('BG')):
-	#	remove_child(bgnode)
-	#	pass
+	if node.is_a_parent_of(get_node('BG')):
+		node.remove_child(bgnode)
 	
-	bgnode = Sprite.new() # Create a new sprite node.
-	bgnode.set_name('BG') # Give it the name BG.
-	add_child_below_node(node, bgnode) # Add BG below the given node.
+	# If of type image make the background a sprite.
+	if type == 'image':
+		bgnode = Sprite.new() # Create a new sprite node.
+		bgnode.set_name('BG') # Give it the name BG.
+		add_child_below_node(node, bgnode) # Add BG below the given node.
 	
-	bgnode.texture = bg # Give bgnode the 'bg' image.s
-	bgnode.centered = false # Uncenter the background.
-	bgnode.z_index = 0 # Send the background to the 	back layer.
-	bgnode.scale = Vector2(global.size.x/1920, global.size.y/1080) # Scale the backgrund to the global size.
+		bgnode.texture = bg # Give bgnode the 'bg' image.s
+		bgnode.centered = false # Uncenter the background.
+		bgnode.z_index = 0 # Send the background to the back layer.
+		bgnode.scale = Vector2(global.size.x/1920, global.size.y/1080) # Scale the backgrund to the global size.
+		
+	# If of type video make the background a videoplayer.
+	if type == 'video':
+		bgnode = VideoPlayer.new() # Create a new videoplayer node.
+		bgnode.set_name('BG') # Give it the name BG.
+		add_child_below_node(node, bgnode) # Add BG below the given node.
+		
+		bgnode.stream = bg
+		bgnode.rect_size = global.size
+		bgnode.connect("finished", self, "loopvideo", [bgnode])
+		bgnode.play()
+		
+func loopvideo(node):
+	node.play()
