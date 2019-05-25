d93b30619974e76e8f14cf932cf1f6b6189d835a
remedee <zacharydanieldowns@gmail.com>
2019-05-25 10:32:02 -0500

    Sound System 1.0.0
    The first complete version of the game's sound system.

--- scripts/systems.gd
+++ scripts/systems.gd
@@ -2,6 +2,7 @@ extends Node
 
 # Node names.
 var display
+var sound
 
 # Load scripts.
 var window = load("res://scripts/windowsettings.gd").new() # Variable to use functions from windowsettings.
@@ -13,7 +14,13 @@ func _ready():
 	global.rootnode = get_node('.').owner
 	
 	# Load the image system under the display variable.
-	display = Sprite.new() # Create a new sprite node.
+	display = Sprite.new() # Create a new Sprite node.
 	display.set_name('Display') # Give it the name Display.
 	display.set_script(load('res://scripts/display.gd')) # Attatch the display script.
 	add_child(display) # Add the node under the Systems node.
+	
+	# Load the sound system under the sound variable.
+	sound = Node.new() # Create a new Node node.
+	sound.set_name('Sound') # Give it the name Sound.
+	sound.set_script(load('res://scripts/sound.gd')) # Attatch the sound script.
+	add_child(sound) # Add the node under the Systems node.
