ca89a21af95633bebd582c4f315706f5c019f07d
DistractedMOSFET <59909722+distractedmosfet@users.noreply.github.com>
2020-03-12 13:43:47 +1300

    More UI work. Including some optimizations.

--- scenes/UI/save_background.gd
+++ scenes/UI/save_background.gd
@@ -8,29 +8,29 @@ enum Track {
 	Out,
 	Idle
 }
+
+#So they don't get ARC'd
+var old_frames = {}
+
 var cur_track = Track.In
 var frame_num = 0
 
 const FRAME_TIME = 1.0/30.0
 var timer = 0.0
 
-#So they don't get ARC'd
-var old_frames = {}
-
 #
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
+func menu_in():
+	cur_track = Track.In
+	frame_num = 0
+
 func menu_out():
 	cur_track = Track.Out
 	frame_num = 0
 
-func menu_in():
-	cur_track = Track.In
-	frame_num = 0
-	texture = null
-	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if not visible:
@@ -47,24 +47,22 @@ func _process(delta):
 					cur_track = Track.Idle
 					frame_num = 0
 			Track.Out:
-				if frame_num >= 18:
+				if frame_num >= 17:
 					emit_signal("outro_finished")
-					frame_num = 0
+					frame_num = 17
 			Track.Idle:
-				frame_num = frame_num%62
-		var path = "res://images/UI/In-game menus/Settings menu/"
+				frame_num = frame_num%61
+		var path = "res://images/UI/In-game menus/"
 		var num_offset = 0
 		#Done seperately so the above match can change tracks
 		match cur_track:
 			Track.In:
-				path += "In/Main_"
+				path += "Save menu/In/Save menu exporter_"
 			Track.Out:
-				#for node in $"../NewButtons".get_children():
-				#	node.visible = false
-				path += "Out/Main_"
+				path += "Save menu/Out/Out_"
 			Track.Idle:
 				num_offset = 18
-				path += "Loop/Main_"
+				path += "Save menu/Loop/Main_"
 		var num_text = str(frame_num+num_offset)
 		while num_text.length() < 5:
 			num_text = "0"+num_text
