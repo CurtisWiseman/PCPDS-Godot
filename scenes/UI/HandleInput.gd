4da66cc11ce44de68ac3f8c7bfc86da15d166ec5
DistractedMOSFET <59909722+distractedmosfet@users.noreply.github.com>
2020-04-02 08:11:05 +1300

    Prevent name issues via restricting permitted characters

--- scenes/UI/HandleInput.gd
+++ scenes/UI/HandleInput.gd
@@ -8,7 +8,9 @@ var placeholder = null;
 var position = null;
 var blink = false;
 
+
 func _ready():
+	connect("text_changed", self, "_sanitize_name")
 	
 	self.add_font_override("normal_font", global.defaultFont)
 	
@@ -21,6 +23,16 @@ func _ready():
 	if placeholder != null: placeholder_text = placeholder
 	if blink: caret_blink = true;
 
+func _sanitize_name(new_text):
+	var res = ""
+	var cursor_pos = caret_position
+	for c in text.lstrip(" "):
+		var char_code = c.ord_at(0)
+		if c.is_valid_integer() or ('a'.ord_at(0) <= char_code and char_code <= 'z'.ord_at(0)) or ('A'.ord_at(0) <= char_code and char_code <= 'Z'.ord_at(0)) or c == " ":
+			res += c
+	text = res
+	caret_position = min(cursor_pos, text.length())
+	
 func _input(event):
 	if Input.is_key_pressed(KEY_ENTER) and text != "":
 		if regex != null:
